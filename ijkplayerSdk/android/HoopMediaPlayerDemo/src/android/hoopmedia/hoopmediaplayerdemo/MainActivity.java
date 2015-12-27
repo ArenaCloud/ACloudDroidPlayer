deea681ab4913b3fa29922bb1ea7066a721a05c1
wanhaiping <916173921@qq.com>
2015-12-27 20:03:20 +0800

    update so and jar

--- ijkplayerSdk/android/HoopMediaPlayerDemo/src/android/hoopmedia/hoopmediaplayerdemo/MainActivity.java
+++ ijkplayerSdk/android/HoopMediaPlayerDemo/src/android/hoopmedia/hoopmediaplayerdemo/MainActivity.java
@@ -3,6 +3,7 @@ package android.hoopmedia.hoopmediaplayerdemo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,18 +19,21 @@ public class MainActivity extends Activity {
         Button btn = (Button) this.findViewById(R.id.StartPlay1);
         final EditText et = (EditText) this.findViewById(R.id.RtmpAddress1);
 
-        et.setText("rtmp://live.hkstv.hk.lxdns.com/live/hks");
+        et.setText("http://test-kw-vod.arenacdn.com/prod/n2hxVa0M8lBPEAZe/index_vod.m3u8");
         
-
         btn.setOnClickListener(new OnClickListener(){
 
         	public void onClick(View v) {
 				
+        		 Log.i("debug", "onClick point1");
         		rtmpAddress = et.getText().toString();
+        		
+        		 Log.i("debug", "onClick point2");
         		
         		Intent intent = new Intent(MainActivity.this, VideoPlayerActivity.class);
         		intent.putExtra("RtmpAddress", rtmpAddress);
         		startActivity(intent);
+        		 Log.i("debug", "onClick point3");
 			}
         });
 	}
--- ijkplayerSdk/android/HoopMediaPlayerDemo/src/android/hoopmedia/hoopmediaplayerdemo/VideoPlayerActivity.java
+++ ijkplayerSdk/android/HoopMediaPlayerDemo/src/android/hoopmedia/hoopmediaplayerdemo/VideoPlayerActivity.java
@@ -14,7 +14,10 @@
  * limitations under the License.
  */
 
+
 package android.hoopmedia.hoopmediaplayerdemo;
+
+/*
 
 import tv.danmaku.ijk.media.widget.MediaController;
 import tv.danmaku.ijk.media.widget.VideoView;
@@ -76,5 +79,149 @@ public class VideoPlayerActivity extends Activity {
 			finish();
 		}
 	};
+}*/
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IMediaPlayer.OnCompletionListener;
+import tv.danmaku.ijk.media.player.IMediaPlayer.OnErrorListener;
+import tv.danmaku.ijk.media.player.IMediaPlayer.OnPreparedListener;
+import tv.danmaku.ijk.media.widget.MediaController;
+import tv.danmaku.ijk.media.widget.VideoView;
+
+public class VideoPlayerActivity extends Activity {
+    private VideoView mVideoView;
+    private View mBufferingIndicator;
+    //private MediaController mMediaController;
+
+    private String mVideoPath;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        Log.i("debug", "onCreate");
+        
+       // mVideoPath = getIntent().getStringExtra("videoPath");
+        mVideoPath = getIntent().getExtras().getString("RtmpAddress");
+
+        mBufferingIndicator = findViewById(R.id.buffering_indicator);
+       // mMediaController = new MediaController(this);
+
+        mVideoView = (VideoView) findViewById(R.id.video_view);
+        
+        mVideoView.setOnPreparedListener(mPreparedListener);
+        mVideoView.setOnErrorListener(mErrorListener);
+        mVideoView.setOnCompletionListener(mCompletionListener);
+        
+       // mVideoView.setDataSourceType(VideoView.LOWDELAY_LIVE_STREAMING_TYPE);
+        mVideoView.setDataCache(6000);
+        
+       // mVideoView.setMediaController(mMediaController);
+        mVideoView.setMediaBufferingIndicator(mBufferingIndicator);
+        
+        mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_ZOOM);
+        
+        mVideoView.setDataSourceType(VideoView.VOD_STREAMING_TYPE);
+        Log.i("debug", "onCreate point1");
+        mVideoView.setVideoPath(mVideoPath);
+        Log.i("debug", "onCreate point2");
+//        startPrint();
+    }
+    
+	private HandlerThread handlerThread=null;
+	private Handler handler=null;
+	private Runnable runnable=null;
+	public void startPrint()
+	{
+		if(handlerThread==null)
+		{
+			handlerThread = new HandlerThread("PrintAbsoluteTimestamp");
+			handlerThread.start();
+			Looper looper = handlerThread.getLooper();
+			handler = new Handler(looper);
+			
+			runnable = new Runnable() {
+				
+				@Override
+				public void run() {
+					Log.v("PrintAbsoluteTimestamp", String.valueOf(mVideoView.getAbsoluteTimestamp()));
+					handler.postDelayed(runnable, 3*1000);
+				}
+			};
+			
+			handler.postDelayed(runnable, 3*1000);
+		}
+	}
+	
+	public void endPrint()
+	{
+		if (handlerThread!=null) {
+			
+			if (handler!=null) {
+				handler.removeCallbacks(runnable);
+				handler = null;
+			}
+			
+			handlerThread.quit();
+			handlerThread = null;
+		}
+	}
+    
+	private OnPreparedListener mPreparedListener = new OnPreparedListener() {
+
+		@Override
+		public void onPrepared(IMediaPlayer mp) {
+			Log.v("VideoPlayerActivity", "onPrepared");
+		}
+	};
+	
+	private OnErrorListener mErrorListener = new OnErrorListener() {
+		@Override
+		public boolean onError(IMediaPlayer mp, int what, int extra) {
+			mVideoView.stopPlayback();
+ 			finish();
+			return true;
+		}
+	};
+
+	private OnCompletionListener mCompletionListener = new OnCompletionListener() {
+		@Override
+		public void onCompletion(IMediaPlayer mp) {
+			mVideoView.stopPlayback();
+			finish();
+		}
+	};
+	
+    @Override  
+    protected void onResume() {
+        super.onResume();
+    }
+    
+    @Override  
+    protected void onPause() {
+        super.onPause();
+    }
+    
+    @Override
+    protected void onDestroy()
+    {
+    	super.onDestroy();
+//    	endPrint();
+    }
 }
+
+
+
+
+
 
